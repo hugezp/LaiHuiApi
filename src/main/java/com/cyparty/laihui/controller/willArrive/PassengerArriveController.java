259772bb61914e9f1d549bfce456721a9a97b314
AnYangWuYanZu <842154692@qq.com>
2017-06-20 15:38:54 +0800

    no message

---
+++ src/main/java/com/cyparty/laihui/controller/willArrive/PassengerArriveController.java
@@ -0,0 +1,16 @@
+package com.cyparty.laihui.controller.willArrive;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 必达单乘客
+ * Created by YangGuang on 2017/6/20.
+ */
+@RequestMapping(value = "/api/app", method = RequestMethod.POST)
+@Controller
+public class PassengerArriveController {
+
+
+}
